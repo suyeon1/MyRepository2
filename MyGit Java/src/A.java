5ca07469c45db481b1c7ff386c4400278a459396
suyeon <thdcjsryghl@naver.com>
2020-06-15 21:08:38 +0900

    First commit in devB branch

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -2,7 +2,8 @@ public class A {
 	public static void main(String args[]) {
 		System.out.println("Say what you want to eat!");
 		System.out.println("what is it??");
-		new B().saylikefood();
+		B b = new B();
+		b.saylikefood();
 	}
 
 }
--- MyGit Java/src/B.java
+++ MyGit Java/src/B.java
@@ -1,6 +1,8 @@
 public class B {
 	public void saylikefood() {
 		System.out.println("¶±ººÀÌ");
+		System.out.println("Ä¡Å²");
+		System.out.println("ÂÌ¸é");
 	}
 
 }
