16e115bec6377010c4635609ab71590729948dfa
suyeon <thdcjsryghl@naver.com>
2020-06-15 21:38:20 +0900

    Second commit in devC branch

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -4,7 +4,8 @@ public class A {
 		System.out.println("what is it??");
 		new B().saylikefood();
 		System.out.println("Change in master branch");
-		new C().saydislikefood();
+		C c = new C();
+		c.saydislikefood();
 
 	}
 
--- MyGit Java/src/C.java
+++ MyGit Java/src/C.java
@@ -1,5 +1,7 @@
 public class C {
 	public void saydislikefood() {
 		System.out.println("Tripe");
+		System.out.println("Raw Dishes");
+		System.out.println("eggplant");
 	}
 }
