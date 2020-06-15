5105f881f973dae099c144fd9a9c6c45adf60875
suyeon <thdcjsryghl@naver.com>
2020-06-15 21:15:29 +0900

    change in master branch

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -3,6 +3,7 @@ public class A {
 		System.out.println("Say what you want to eat!");
 		System.out.println("what is it??");
 		new B().saylikefood();
+		System.out.println("Change in master branch");
 	}
 
 }
--- MyGit Java/src/B.java
+++ MyGit Java/src/B.java
@@ -1,6 +1,7 @@
 public class B {
 	public void saylikefood() {
 		System.out.println("∂±∫∫¿Ã");
+		System.out.println("saylikefood change in master branch");
 	}
 
 }
